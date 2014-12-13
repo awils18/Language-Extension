0a4d0b9e7f6c4f7710e7b16081169bd519e5751b
awils18 <awils18@u.rochester.edu>
2014-12-13 17:13:31 -0500

    Basic framework

---
+++ tree.py
@@ -0,0 +1,24 @@
+import sys
+
+
+class Node:
+    
+    def __init__(self):
+        
+        
+class Tree:
+    
+    def __init__(self):
+        
+    def insert(self):
+        
+    def delete(self):
+        
+        
+
+        
+    
+        
+    
+        
+        
