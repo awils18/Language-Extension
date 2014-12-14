ed05c90033c60e8a49fbfca049bc9263069be430
awils18 <awils18@u.rochester.edu>
2014-12-13 21:02:47 -0500

    Shared lib attempt

--- tree.py
+++ tree.py
@@ -1,18 +1,31 @@
 import sys
+import ctypes
+from ctypes import cdll
+
+testlib = ctypes.CDLL('C:\Users\awils18\Documents\GitHub\Language-Extension\interval_tree.so')
 
 
-class Node:
+
+
+
+
+
+
+#class Node():
+    #def __init__(self):
+        #self.start = 0
+        #self.end = 0
+        #self.maxValue = 0
+        
+        
+#class IntervalTree():
     
-    def __init__(self):
+    #def __init__(self):
+        #self.yolo = 0
         
+    #def insertNode(self):
         
-class Tree:
-    
-    def __init__(self):
-        
-    def insert(self):
-        
-    def delete(self):
+    #def deleteNode(self):
         
         
 
